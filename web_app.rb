01ad66bdbae111af1dc2de056c02de950077e49b
Raphael Molesim <raphael.molesim@happy.co>
2024-05-01 11:27:56 -0300

    Wip

--- web_app.rb
+++ web_app.rb
@@ -1,14 +1,21 @@
-require 'sinatra'
-require 'json'
-require './scryfall'
+require "sinatra"
+require "json"
+require "./scryfall"
 
 set :port, 3001
 
-get '/sets' do
+set :public_folder, __dir__ + '/out'
+
+get "/sets" do
   cards_files = Dir["./db/*-cards.json"]
   scryfall = Scryfall.new
   sets = cards_files.map { |file| file.split("/").last.split("-").first }.select
   sets = sets.map { |set| scryfall.get_set(set) }
-    .select { |set| set.parent_set_code == nil }.map { |set| set.to_json }
+    .select { |set| set.parent_set_code.nil? }.map { |set| set.to_json }
   sets.to_json
 end
+
+get "/" do
+  File.read("views/index.html")
+end
+
