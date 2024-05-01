e79f8887d2947a02f75d3e729fad2f75d64ee3fa
Raphael Molesim <raphael.molesim@happy.co>
2024-05-01 00:27:45 -0300

    Wip

--- web_app.rb
+++ web_app.rb
@@ -7,7 +7,8 @@ set :port, 3001
 get '/sets' do
   cards_files = Dir["./db/*-cards.json"]
   scryfall = Scryfall.new
-  sets = cards_files.map { |file| file.split("/").last.split("-").first }
-  sets = sets.map { |set| scryfall.get_set(set).to_json }
+  sets = cards_files.map { |file| file.split("/").last.split("-").first }.select
+  sets = sets.map { |set| scryfall.get_set(set) }
+    .select { |set| set.parent_set_code == nil }.map { |set| set.to_json }
   sets.to_json
 end
