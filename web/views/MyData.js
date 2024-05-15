069bed87ca82ab866abe7120fffb93370a0bc7c3
Raphael Molesim <raphael.molesim@happy.co>
2024-05-15 11:07:29 -0300

    UI improvements in My Data page

--- web/views/MyData.js
+++ web/views/MyData.js
@@ -12,7 +12,12 @@ const MyData = () => {
   }, [])
   
   const renderStatus = (status) => {
-    return status;
+    const cssColor = status === "open" ? "bg-stone-500" : "bg-cyan-700";
+    return (
+      <span className={`text-xs rounded-lg	text-center ${cssColor} px-2 w-[170px] text-slate-100 font-bold pb-[2px]`}>
+        {status}
+      </span>
+    );
   };
 
   const renderRatingSeries = (ratingSeries) => {
@@ -31,14 +36,14 @@ const MyData = () => {
       return (<ul>
         <li className="pt-8 text-base font-bold">Outlaws of the Thunder Junction Block</li>
         <ul>
-          {ratingSeriesList.map((series) => {
+          {ratingSeriesList.map((series, idx) => {
             const ratings = series.series
-            console.log(ratings, ratings.filter((rating) => rating.conclusion == "true").length,  ratings.length);
-            const performance = (ratings.filter((rating) => rating.conclusion == "true").length / ratings.length) * 100.0;
+            const percentage = (ratings.filter((rating) => rating.conclusion == "true").length / ratings.length) * 100.0;
+            const performance = percentage.toFixed(0);
             return (
-              <li key={series.id} className="pl-8 flex flex-rom">
+              <li key={series.id} className="pl-4 flex flex-rom">
                 <div className="grow">
-                  Performance:
+                  Series {(idx + 1)}
                 </div>
                 <div className="grow">
                   {renderStatus(series.status)}
@@ -57,7 +62,7 @@ const MyData = () => {
   return (
     <MasterPage>
       <div className="p-4">
-        <h1 className="text-4xl font-bold pt-4">My Data</h1>
+        <h1 className="text-4xl font-bold">My Data</h1>
         <ul>
           {renderRatingSeries(ratingSeries)}
         </ul>
